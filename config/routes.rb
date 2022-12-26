8d0ed32e1d652a0b82f6ec5bf54d00e3a718a8ce
João Souza <joaomarcelo9393@gmail.com>
2022-12-25 23:12:31 -0300

    Create User model with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   root controller: :menus, action: :index
+
   resources :menus
   resources :menu_items, only: %i[ create update destroy ]
 end
