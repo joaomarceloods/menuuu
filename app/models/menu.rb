2f482bc7604649b6f38994298f1fac09718102e9
João Souza <joaomarcelo9393@gmail.com>
2023-01-05 14:03:07 -0300

    Show publicly visible MenuItems on public Menu

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,5 +3,10 @@ class Menu < ApplicationRecord
 
   has_many :menu_items
 
+  has_many :publicly_visible_menu_items, -> {
+    where.not(price: nil).
+    where.not("TRIM(name) = ''")
+  }, class_name: "MenuItem"
+
   validates :name, presence: true
 end
