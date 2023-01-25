4e1b68ea942358947b02677bb1a9e9c2f908f669
João Souza <joaomarcelo9393@gmail.com>
2023-01-25 01:38:00 -0300

    Seed the development database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+if Rails.env.development?
+  ActiveRecord::Base.transaction do
+    user = User.create!(email: "jon@doe", password: "123123")
+
+    business = Business.create!(user: user, name: "Doe's")
+
+    menu = Menu.create!(business: business, name: "Menu")
+
+    menu.menu_items.create!(Private::CreateDemonstrativeMenu::DEMONSTRATIVE_MENU_ITEMS)
+  end
+end
