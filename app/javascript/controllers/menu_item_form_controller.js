9048f977266e0f49c41d09d38f1be535e88cf35e
João Souza <joaomarcelo9393@gmail.com>
2023-04-27 13:55:01 -0300

    Format price with two decimals

--- app/javascript/controllers/menu_item_form_controller.js
+++ app/javascript/controllers/menu_item_form_controller.js
@@ -4,13 +4,20 @@ import debounce from "lodash.debounce"
 export default class extends Controller {
   connect() {
     this.input = debounce(this.input, 300).bind(this)
+    this.#formatPrice()
   }
 
   // input->menu-item-form#input
   input() {
+    this.#formatPrice()
     // Turbo Drive requires usage of requestSubmit() instead of submit().
     // https://turbo.hotwired.dev/handbook/drive#form-submissions
     this.element.requestSubmit()
+  }
+
+  #formatPrice() {
+    const priceInput = this.element.querySelector(".menu-item__price")
+    priceInput.value = parseFloat(priceInput.value).toFixed(2)
   }
 
   // keypress:enter->menu-item-form#keypressEnter
@@ -21,7 +28,7 @@ export default class extends Controller {
   // Source: https://stackoverflow.com/a/35173443/4668975
   #focusOnNextInput() {
     if (document.activeElement) {
-      const focussableElementsQuery = 'input[type=text]:not([disabled])';
+      const focussableElementsQuery = 'input:not([disabled])';
 
       const focussableElements = [...document.querySelectorAll(focussableElementsQuery)].filter(
         element => element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement
