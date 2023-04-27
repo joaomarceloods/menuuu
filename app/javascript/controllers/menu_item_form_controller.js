020e6331f37012e824035b246de01783f4afd586
João Souza <joaomarcelo9393@gmail.com>
2023-04-27 17:58:08 -0300

    Add menu items by pressing Enter

--- app/javascript/controllers/menu_item_form_controller.js
+++ app/javascript/controllers/menu_item_form_controller.js
@@ -2,6 +2,11 @@ import { Controller } from "@hotwired/stimulus"
 import debounce from "lodash.debounce"
 
 export default class extends Controller {
+  static targets = [
+    "newForm",
+    "editForm",
+  ]
+
   connect() {
     this.input = debounce(this.input, 300).bind(this)
     this.#formatPrice()
@@ -12,35 +17,19 @@ export default class extends Controller {
     this.#formatPrice()
     // Turbo Drive requires usage of requestSubmit() instead of submit().
     // https://turbo.hotwired.dev/handbook/drive#form-submissions
-    this.element.requestSubmit()
+    this.editFormTarget?.requestSubmit()
   }
 
   #formatPrice() {
-    const priceInput = this.element.querySelector(".menu-item-price")
-    if (!priceInput) return
+    if (!this.hasEditFormTarget) return
+    const priceInput = this.editFormTarget.querySelector(".menu-item-price")
+    if (!priceInput || !priceInput.length) return
     priceInput.value = parseFloat(priceInput.value).toFixed(2)
   }
 
   // keypress:enter->menu-item-form#keypressEnter
   keypressEnter() {
-    this.#focusOnNextInput()
-  }
-
-  // Source: https://stackoverflow.com/a/35173443/4668975
-  #focusOnNextInput() {
-    if (document.activeElement) {
-      const focussableElementsQuery = 'input:not([disabled])';
-
-      const focussableElements = [...document.querySelectorAll(focussableElementsQuery)].filter(
-        element => element.offsetWidth > 0 || element.offsetHeight > 0 || element === document.activeElement
-      )
-
-      const index = focussableElements.indexOf(document.activeElement)
-
-      if (index > -1) {
-        const nextElement = focussableElements[index + 1] || focussableElements[0]
-        nextElement.focus()
-      }
-    }
+    // this.#focusOnNextInput()
+    this.newFormTarget?.requestSubmit()
   }
 }
