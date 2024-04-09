0a5db3d50c2a3d810a5fa85de1589c913526bdb4
João Souza <joaomarcelo9393@gmail.com>
2024-04-09 12:27:40 -0300

    Spec Authentication

---
+++ spec/features/private/authentication_spec.rb
@@ -0,0 +1,59 @@
+require 'rails_helper'
+
+RSpec.feature "Authentication", type: :feature do
+  subject { page }
+  let(:user) { User.create!(email: "user@example.com", password: "password") }
+
+  describe "root" do
+    context "unauthenticated" do
+      before { visit "/" }
+      it { is_expected.to have_text("Log in") }
+    end
+
+    context "authenticated" do
+      before { sign_in user }
+      before { visit "/" }
+      it { is_expected.to have_text("What's your business' name?") }
+    end
+  end
+
+  describe "sign up" do
+    context "authenticated" do
+      before { sign_in user }
+      before { visit "/users/sign_up" }
+      it { is_expected.to have_text("You are already signed in.") }
+    end
+
+    context "unauthenticated" do
+      before do
+        visit "/users/sign_up"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "password"
+        fill_in "Password confirmation", with: "password"
+        click_button "Sign up"
+      end
+
+      it { is_expected.to have_text("You have signed up successfully.") }
+    end
+  end
+
+  describe "sign in" do
+    context "authenticated" do
+      before { sign_in user }
+      before { visit "/users/sign_in" }
+      it { is_expected.to have_text("You are already signed in.") }
+    end
+
+    context "unauthenticated" do
+      before do
+        user
+        visit "/users/sign_in"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "password"
+        click_button "Log in"
+      end
+
+      it { is_expected.to have_text("Signed in successfully.") }
+    end
+  end
+end
