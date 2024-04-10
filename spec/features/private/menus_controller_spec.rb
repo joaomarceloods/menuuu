26be16cc52aa13be3e6875f76a70cf29777727aa
João Souza <joaomarcelo9393@gmail.com>
2024-04-10 11:54:02 -0300

    Rearrange spec contexts

--- spec/features/private/menus_controller_spec.rb
+++ spec/features/private/menus_controller_spec.rb
@@ -8,8 +8,10 @@ RSpec.feature "Private::MenusControllers", type: :feature do
 
   describe "#index" do
     context "unauthenticated" do
-      before { visit "/private/menus" }
-      it { is_expected.to have_text("You need to sign in or sign up before continuing.") }
+      describe "content" do
+        before { visit "/private/menus" }
+        it { is_expected.to have_text("You need to sign in or sign up before continuing.") }
+      end
     end
 
     context "authenticated" do
@@ -53,67 +55,92 @@ RSpec.feature "Private::MenusControllers", type: :feature do
 
   describe "#show" do
     context "unauthenticated" do
-      before { visit "/private/menus/1" }
-      it { is_expected.to have_text("You need to sign in or sign up before continuing.") }
+      describe "content" do
+        before { visit "/private/menus/1" }
+        it { is_expected.to have_text("You need to sign in or sign up before continuing.") }
+      end
     end
 
     context "authenticated" do
       before { sign_in user }
 
       context "without business" do
-        before { visit "/private/menus/1" }
-        it { is_expected.to have_text("What's your business' name?") }
+        describe "content" do
+          before { visit "/private/menus/1" }
+          it { is_expected.to have_text("What's your business' name?") }
+        end
       end
 
       context "with business" do
         before { business }
 
-        context "with menu" do
-          before { menu }
-          before { visit "/private/menus/#{menu.id}" }
-
+        context "without menu" do
           describe "content" do
-            it { is_expected.to have_field("Menu name", with: "My Menu") }
-            it { is_expected.to have_no_field("Section name") }
-            it { is_expected.to have_no_button("Add item…") }
-          end
-
-          describe "renaming menu", js: true do
-            before { fill_in "Menu name", with: "New Menu Name" }
-            before { refresh }
-            it { is_expected.to have_field("Menu name", with: "New Menu Name") }
-          end
-
-          describe "adding section" do
-            before { click_button "Add section…" }
-            it { is_expected.to have_field("Section name", count: 1) }
-            it { is_expected.to have_button("Add item", count: 1) }
-
-            describe "adding another section" do
-              before { click_button "Add section…", match: :first }
-              it { is_expected.to have_field("Section name", count: 2) }
-              it { is_expected.to have_button("Add item", count: 2) }
+            it "raises" do
+              expect { visit "/private/menus/1" }.to raise_error(ActiveRecord::RecordNotFound)
             end
+          end
+        end
+
+        context "with menu" do
+          let(:path) { "/private/menus/#{menu.id}" }
+
+          context "without section" do
+            describe "content" do
+              before { visit path }
+              it { is_expected.to have_field("Menu name", with: "My Menu") }
+              it { is_expected.to have_no_field("Section name") }
+              it { is_expected.to have_no_button("Add item…") }
+            end
+
+            describe "renaming menu", js: true do
+              before { visit path }
+              before { fill_in "Menu name", with: "New Menu Name" }
+              before { refresh }
+              it { is_expected.to have_field("Menu name", with: "New Menu Name") }
+            end
+
+            describe "adding section" do
+              before { visit path }
+              before { click_button "Add section…" }
+              it { is_expected.to have_field("Section name", count: 1) }
+              it { is_expected.to have_button("Add item", count: 1) }
+            end
+          end
+
+          context "with section" do
+            let!(:section) { MenuSection.create!(name: "My Section", menu: menu) }
 
             describe "updating section", js: true do
-              before do
-                fill_in "Section name", with: "My Section"
-                sleep 0.5
-                refresh
-              end
-              it { is_expected.to have_field("Section name", with: "My Section") }
+              before { visit path }
+              before { fill_in "Section name", with: "New section name" }
+              before { sleep 0.5 }
+              before { refresh }
+              it { is_expected.to have_field("Section name", with: "New section name") }
             end
 
-            describe "adding item" do
-              before { click_button "Add item" }
-              it { is_expected.to have_field("menu_item_name", count: 1) }
-              it { is_expected.to have_field("menu_item_price", count: 1) }
+            describe "adding another section" do
+              before { visit path }
+              before { click_button "Add section…", match: :first }
+              it { is_expected.to have_field("Section name", count: 2) }
+            end
 
-              describe "adding another item" do
-                before { click_button "Add item" }
-                it { is_expected.to have_field("menu_item_name", count: 2) }
-                it { is_expected.to have_field("menu_item_price", count: 2) }
+            context "without item" do
+              before { visit path }
+
+              describe "content" do
+                it { is_expected.to have_no_field("menu_item_name") }
               end
+
+              describe "adding item", js: true do
+                before { click_button "Add item" }
+                it { is_expected.to have_field("menu_item_name", count: 1) }
+              end
+            end
+
+            context "with one item" do
+              let!(:item) { MenuItem.create!(name: "My Item", price: 10.00, menu_section: section) }
+              before { visit path }
 
               describe "updating item", js: true do
                 before do
@@ -123,41 +150,32 @@ RSpec.feature "Private::MenusControllers", type: :feature do
                   refresh
                 end
 
-                it "updates the item name and price" do
-                  expect(page).to have_field("menu_item_name", with: "My Item")
-                  expect(page).to have_field("menu_item_price", with: "10.00")
-                end
+                it { is_expected.to have_field("menu_item_name", with: "My Item") }
+                it { is_expected.to have_field("menu_item_price", with: "10.00") }
               end
 
               describe "deleting item", js: true do
                 before do
                   find_field("menu_item_name").click
                   within ".menu-item .action-bar" do
-                    accept_confirm "Remove the empty item permanently?" do
+                    accept_confirm "Remove the My Item item permanently?" do
                       click_button "close"
                     end
                   end
                 end
+
                 it { is_expected.to have_no_field("menu_item_name") }
-                it { is_expected.to have_no_field("menu_item_price") }
               end
             end
-          end
-
-          context "with section" do
-            let(:section) { MenuSection.create!(name: "My Section", menu: menu) }
-
-            context "with one item" do
-              let(:item) { MenuItem.create!(name: "My Item", price: 10.00, menu_section: section) }
 
             context "with two items" do
               let!(:item_1) { MenuItem.create!(name: "Item 1", price: 10.00, menu_section: section) }
               let!(:item_2) { MenuItem.create!(name: "Item 2", price: 20.00, menu_section: section) }
+              before { visit path }
 
               describe "item order" do
                 let(:dom_id_1) { "#menu_item_#{item_1.id}" }
                 let(:dom_id_2) { "#menu_item_#{item_2.id}" }
-                before { visit "/private/menus/#{menu.id}" }
                 it { is_expected.to have_selector("#{dom_id_1} + #{dom_id_2}") }
 
                 describe "dragging item 1 before item 2", js: true do
@@ -174,5 +192,4 @@ RSpec.feature "Private::MenusControllers", type: :feature do
       end
     end
   end
-end
 end
