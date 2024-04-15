ab49a3ad3d0de650fe10a8e47a3661ab146b0487
João Souza <joaomarcelo9393@gmail.com>
2024-04-15 17:23:24 -0300

    Change default menu

--- app/services/menu/build_default.rb
+++ app/services/menu/build_default.rb
@@ -1,12 +1,12 @@
 class Menu::BuildDefault < ApplicationService
   def call(menu_params)
-    Menu.new(menu_params.merge(name: "My Menu (edit me)")).tap do |menu|
-      menu.menu_sections.build(menu:, name: "Some Tips For You (edit me)").tap do |menu_section|
+    Menu.new(menu_params.merge(name: "My Menu")).tap do |menu|
+      menu.menu_sections.build(menu:, name: "Tips Section").tap do |menu_section|
         menu_section.menu_items.build([
           { menu_section:, name: "Rename this item" },
           { menu_section:, name: "Change price --->", price: 10 },
+          { menu_section:, name: "Change the section title" },
           { menu_section:, name: "Rename the menu" },
-          { menu_section:, name: "Rename the section above" },
           { menu_section:, name: "Drag items & sections around" },
         ])
       end
--- spec/features/private/authentication_spec.rb
+++ spec/features/private/authentication_spec.rb
@@ -62,13 +62,17 @@ RSpec.feature "Authentication", type: :feature do
           click_button "Next"
         end
 
-        it { is_expected.to have_field("Menu name", with: "My Menu (edit me)") }
-        it { is_expected.to have_field("Enter section name…", with: "Some Tips For You (edit me)") }
-        it { is_expected.to have_field("Enter item name…", with: "Rename this item") }
-        it { is_expected.to have_field("Enter item name…", with: "Change price --->") }
-        it { is_expected.to have_field("Enter item name…", with: "Rename the menu") }
-        it { is_expected.to have_field("Enter item name…", with: "Rename the section above") }
-        it { is_expected.to have_field("Enter item name…", with: "Drag items & sections around") }
+        it "has fields" do
+          expect(subject).to have_field("Menu name", with: "My Menu")
+          expect(subject).to have_field("Enter section name…", with: "Tips Section")
+          expect(page.all('input#menu_item_name').map(&:value)).to eq([
+            "Rename this item",
+            "Change price --->",
+            "Change the section title",
+            "Rename the menu",
+            "Drag items & sections around",
+          ])
+        end
       end
     end
   end
