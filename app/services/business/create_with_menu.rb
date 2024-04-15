d45aef1d9b8222043d90b02766b45f88a92f3160
João Souza <joaomarcelo9393@gmail.com>
2024-04-15 16:01:09 -0300

    Add instructions to default menu

--- app/services/business/create_with_menu.rb
+++ app/services/business/create_with_menu.rb
@@ -1,27 +1,19 @@
 class Business::CreateWithMenu < ApplicationService
-
-  MENU_ITEMS_EATS = [
-    { name: "Cheese Burger", price: "15" },
-    { name: "French Fries", price: "10" },
-  ]
-
-  MENU_ITEMS_DRINKS = [
-    { name: "Soda", price: "5" },
-  ]
-
   def call(business_params)
     business = Business.new(business_params)
 
     ActiveRecord::Base.transaction do
       business.save!
-      menu = business.menus.create!(name: "Menu")
-
-      menu.menu_sections.create!(name: "Eats").tap do |menu_section|
-        menu_section.menu_items.create!(MENU_ITEMS_EATS)
-      end
-
-      menu.menu_sections.create!(name: "Drinks").tap do |menu_section|
-        menu_section.menu_items.create!(MENU_ITEMS_DRINKS)
+      business.menus.create!(name: "My First Menu").tap do |menu|
+        menu.menu_sections.create!(name: "Some Tips For You (edit me)").tap do |menu_section|
+          menu_section.menu_items.create!([
+            { name: "Rename this item" },
+            { name: "Change price --->", price: 10 },
+            { name: "Rename the menu" },
+            { name: "Rename the section above" },
+            { name: "Drag items & sections around" },
+          ])
+        end
       end
     rescue ActiveRecord::RecordInvalid => e
       Rails.logger.debug e
