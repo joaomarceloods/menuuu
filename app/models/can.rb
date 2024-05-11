c688d13d285cf9eaed75ec61a6239ac473898a0c
João Souza <joaomarcelo9393@gmail.com>
2024-05-11 18:56:12 -0300

    Show free tier limit tooltips

--- app/models/can.rb
+++ app/models/can.rb
@@ -8,25 +8,24 @@ class Can
   end
 
   def self.create_menu!(business)
-    raise AuthorizationError unless create_menu?(business)
+    create_menu?(business) or raise AuthorizationError
   end
 
   def self.create_menu_section?(menu)
     limit = menu.business.subscribed? ? 1000 : 10
-    menu.menu.menu_sections.count < limit
+    menu.menu_sections.count < limit
   end
 
   def self.create_menu_section!(menu)
-    raise AuthorizationError unless create_menu_section?(menu)
+    create_menu_section?(menu) or raise AuthorizationError
   end
 
   def self.create_menu_item?(menu)
     limit = menu.business.subscribed? ? 1000 : 10
-    menu.menu.menu_items.count < limit
+    menu.menu_items.count < limit
   end
 
   def self.create_menu_item!(menu)
-    raise AuthorizationError unless create_menu_item?(menu)
+    create_menu_item?(menu) or raise AuthorizationError
   end
-
 end
