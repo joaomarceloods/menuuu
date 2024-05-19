4ab1af67a422b92f5f2a92878038f592a41de803
João Souza <joaomarcelo9393@gmail.com>
2024-05-19 08:33:53 -0300

    Cache home and public menu pages

--- app/controllers/public/home_controller.rb
+++ app/controllers/public/home_controller.rb
@@ -2,5 +2,7 @@ class Public::HomeController < Public::ApplicationController
   def index
     @menu = Business.first.menus.first
     @qrcode = RQRCode::QRCode.new(public_menu_url(@menu))
+    fresh_when last_modified: @menu.updated_at, etag: @menu, public: true
+    expires_in 24.hours, public: true
   end
 end
