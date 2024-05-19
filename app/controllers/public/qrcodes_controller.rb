25a89dd0c88d03220bcfd4ac9dff9d2946643073
João Souza <joaomarcelo9393@gmail.com>
2024-05-19 11:17:49 -0300

    Improve home page ips from 14K to 35K
    ips = iterations per second
    
    Benchmark:
    
    USE_SERVER=puma bundle exec derailed exec perf:ips

--- app/controllers/public/qrcodes_controller.rb
+++ app/controllers/public/qrcodes_controller.rb
@@ -1,6 +1,9 @@
 class Public::QrcodesController < Public::ApplicationController
   def show
-    qrcode = RQRCode::QRCode.new(public_menu_path(params[:business_id]))
-    render body: qrcode.as_svg, content_type: "image/svg+xml"
+    expires_in 24.hours, public: true
+    if stale?(params[:menu_id])
+      qrcode = RQRCode::QRCode.new(public_menu_url(params[:menu_id]))
+      render body: qrcode.as_svg(use_path: true), content_type: "image/svg+xml"
+    end
   end
 end
