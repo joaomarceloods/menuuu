b89db084d1f997816fc8860652135a05bc8e0f2b
João Souza <joaomarcelo9393@gmail.com>
2024-05-29 19:25:29 -0300

    Create demo menus in Spanish and Portuguese

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,21 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-return if User.any?
+user = User.find_by(email: Rails.application.credentials.dig(:admin, :email)) ||
+        User.create!(
+          email: Rails.application.credentials.dig(:admin, :email),
+          password: Rails.application.credentials.dig(:admin, :password),
+        )
 
+business = Business.find_or_create_by!(user:, name: "The Menuuu")
+
+Subscription.find_or_create_by!(business:, stripe_subscription_id: "sub_demo", expired_at: "3000-01-01")
+
+# Create a demo menu in English
 ActiveRecord::Base.transaction do
-  user = User.create!(
-    email: Rails.application.credentials.dig(:admin, :email),
-    password: Rails.application.credentials.dig(:admin, :password),
-  )
+  break if Menu.exists?(demo: true, locale: :en)
 
-  business = Business.create!(user:, name: "The Menuuu")
-  Subscription.create!(business:, stripe_subscription_id: "sub_123456", expired_at: 100.years.from_now)
-  menu = Menu.create!(business:, name: "The Menuuu", published: true, demo: true)
+  menu = Menu.create!(business:, name: "The Menuuu", published: true, demo: true, locale: :en)
 
   MenuSection.create!(menu:, name: "Appetizers") do |menu_section|
     MenuItem.create!(menu_section:, name: "Fried Pickles", price: 5.99)
@@ -51,5 +55,87 @@ ActiveRecord::Base.transaction do
     MenuItem.create!(menu_section:, name: "Cheesecake", price: 4.99)
     MenuItem.create!(menu_section:, name: "Ice Cream", price: 3.99)
     MenuItem.create!(menu_section:, name: "Pie", price: 4.99)
+  end
+end
+
+# Create a demo menu in Spanish
+ActiveRecord::Base.transaction do
+  break if Menu.exists?(demo: true, locale: :es)
+
+  menu = Menu.create!(business:, name: "El Menú", published: true, demo: true, locale: :es)
+
+  MenuSection.create!(menu:, name: "Aperitivos") do |menu_section|
+    MenuItem.create!(menu_section:, name: "Pepinillos Fritos", price: 5.99)
+    MenuItem.create!(menu_section:, name: "Palitos de Mozzarella", price: 6.99)
+    MenuItem.create!(menu_section:, name: "Aros de Cebolla", price: 4.99)
+  end
+
+  MenuSection.create!(menu:, name: "Platos Principales").tap do |menu_section|
+    MenuItem.create!(menu_section:, name: "Costillas", price: 15.99)
+    MenuItem.create!(menu_section:, name: "Pollo", price: 12.99)
+    MenuItem.create!(menu_section:, name: "Cerdo Desmenuzado", price: 13.99)
+    MenuItem.create!(menu_section:, name: "Brisket", price: 14.99)
+    MenuItem.create!(menu_section:, name: "Salchicha", price: 11.99)
+  end
+
+  MenuSection.create!(menu:, name: "Acompañamientos").tap do |menu_section|
+    MenuItem.create!(menu_section:, name: "Papas Fritas", price: 2.99)
+    MenuItem.create!(menu_section:, name: "Macarrones con Queso", price: 3.99)
+    MenuItem.create!(menu_section:, name: "Ensalada de Col", price: 1.99)
+  end
+
+  MenuSection.create!(menu:, name: "Bebidas").tap do |menu_section|
+    MenuItem.create!(menu_section:, name: "Soda", price: 1.99)
+    MenuItem.create!(menu_section:, name: "Cerveza", price: 3.99)
+    MenuItem.create!(menu_section:, name: "Vino", price: 5.99)
+    MenuItem.create!(menu_section:, name: "Cóctel", price: 7.99)
+    MenuItem.create!(menu_section:, name: "Agua", price: 0.99)
+  end
+
+  MenuSection.create!(menu:, name: "Postre").tap do |menu_section|
+    MenuItem.create!(menu_section:, name: "Cheesecake", price: 4.99)
+    MenuItem.create!(menu_section:, name: "Helado", price: 3.99)
+    MenuItem.create!(menu_section:, name: "Pastel", price: 4.99)
+  end
+end
+
+# Create a demo menu in Portuguese
+ActiveRecord::Base.transaction do
+  break if Menu.exists?(demo: true, locale: :pt)
+
+  menu = Menu.create!(business:, name: "O Cardápio", published: true, demo: true, locale: :pt)
+
+  MenuSection.create!(menu:, name: "Aperitivos") do |menu_section|
+    MenuItem.create!(menu_section:, name: "Picles Fritos", price: 5.99)
+    MenuItem.create!(menu_section:, name: "Palitos de Mussarela", price: 6.99)
+    MenuItem.create!(menu_section:, name: "Anéis de Cebola", price: 4.99)
+  end
+
+  MenuSection.create!(menu:, name: "Pratos Principais").tap do |menu_section|
+    MenuItem.create!(menu_section:, name: "Costelas", price: 15.99)
+    MenuItem.create!(menu_section:, name: "Frango", price: 12.99)
+    MenuItem.create!(menu_section:, name: "Carne de Porco Desfiada", price: 13.99)
+    MenuItem.create!(menu_section:, name: "Brisket", price: 14.99)
+    MenuItem.create!(menu_section:, name: "Linguiça", price: 11.99)
+  end
+
+  MenuSection.create!(menu:, name: "Acompanhamentos").tap do |menu_section|
+    MenuItem.create!(menu_section:, name: "Batatas Fritas", price: 2.99)
+    MenuItem.create!(menu_section:, name: "Macarrão com Queijo", price: 3.99)
+    MenuItem.create!(menu_section:, name: "Salada de Repolho", price: 1.99)
+  end
+
+  MenuSection.create!(menu:, name: "Bebidas").tap do |menu_section|
+    MenuItem.create!(menu_section:, name: "Refrigerante", price: 1.99)
+    MenuItem.create!(menu_section:, name: "Cerveja", price: 3.99)
+    MenuItem.create!(menu_section:, name: "Vinho", price: 5.99)
+    MenuItem.create!(menu_section:, name: "Coquetel", price: 7.99)
+    MenuItem.create!(menu_section:, name: "Água", price: 0.99)
+  end
+
+  MenuSection.create!(menu:, name: "Sobremesa").tap do |menu_section|
+    MenuItem.create!(menu_section:, name: "Cheesecake", price: 4.99)
+    MenuItem.create!(menu_section:, name: "Sorvete", price: 3.99)
+    MenuItem.create!(menu_section:, name: "Torta", price: 4.99)
   end
 end
