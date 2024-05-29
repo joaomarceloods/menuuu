54873ea36954e79f4d9cb35e4c82647aeba42061
João Souza <joaomarcelo9393@gmail.com>
2024-05-29 20:44:31 -0300

    Cache public business page

--- app/controllers/public/businesses_controller.rb
+++ app/controllers/public/businesses_controller.rb
@@ -2,6 +2,8 @@ class Public::BusinessesController < Public::ApplicationController
   def show
     @business = Business.find(params[:id])
     @menus = @business.menus.where(published: true)
+    fresh_when @menus
+    expires_in 24.hours, public: true
     redirect_to public_menu_path(@menus.first) if @menus.one? && !coming_from_menu?
   end
 
