c097213fa80da6068919ec0641b49ddc7c76b17e
João Souza <joaomarcelo9393@gmail.com>
2024-06-08 20:10:17 -0300

    Create default menu empty

--- app/services/menu/build_default.rb
+++ app/services/menu/build_default.rb
@@ -1,19 +1,11 @@
 class Menu::BuildDefault < ApplicationService
   def call(menu_params)
     Menu.new(menu_params.merge(name: I18n.t('menu.default.name'))).tap do |menu|
-      menu.menu_sections.build(menu:, name: I18n.t('menu.default.section_1')).tap do |menu_section|
+      menu.menu_sections.build(menu:).tap do |menu_section|
         menu_section.menu_items.build([
-          { menu_section:, name: I18n.t('menu.default.item_1_1'), description: I18n.t('menu.default.description_1_1') },
-          { menu_section:, name: I18n.t('menu.default.item_1_2'), description: I18n.t('menu.default.description_1_2') },
-          { menu_section:, name: I18n.t('menu.default.item_1_3'), description: I18n.t('menu.default.description_1_3') },
-        ])
-      end
-
-      menu.menu_sections.build(menu:, name: I18n.t('menu.default.section_2')).tap do |menu_section|
-        menu_section.menu_items.build([
-          { menu_section:, name: I18n.t('menu.default.item_2_1'), description: I18n.t('menu.default.description_2_1') },
-          { menu_section:, name: I18n.t('menu.default.item_2_2') },
-          { menu_section:, name: I18n.t('menu.default.item_2_3'), price: 9.90 },
+          { menu_section: },
+          { menu_section: },
+          { menu_section: },
         ])
       end
     end
